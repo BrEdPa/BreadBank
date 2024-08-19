f140efe4c5b608e53b553d6f51aaa65d2c06589f
Starch Patriarch <144400608+BrEdPa@users.noreply.github.com>
2024-08-19 15:10:12 -0500

    Update and rename BPDT_desktopAnyConnectRemediation.ps1 to secureClientNetworkRemediation.ps1

---
+++ secureClientNetworkRemediation.ps1
@@ -0,0 +1,22 @@
+#define config file location on network and destination on local machine
+$sourceConfigFileRepositoryPath = "\\Repository\apps\Cisco Secure Client\configFileCopy\"
+$configFileDestinationUmbrella = "C:\ProgramData\Cisco\Cisco Secure Client\Umbrella"
+$configFileDestinationVPN = "C:\ProgramData\Cisco\Cisco Secure Client\VPN\Profile"
+
+#define and store local machine variables for checking work
+$configFileUmbrella = "C:\ProgramData\Cisco\Cisco Secure Client\Umbrella\OrgInfo.json"
+$configFileVPN = "C:\ProgramData\Cisco\Cisco Secure Client\VPN\Profile\Anyconnect.xml"
+$checkResultUmbrella = Test-Path -path $configFileUmbrella
+$checkResultVPN = Test-Path -Path $configFileVPN
+
+If ($checkResultUmbrella -eq $false -or $checkResultVPN -eq $false) {
+#map network drive
+    New-PSDrive -Name "Repository" -PSProvider FileSystem -Root $sourceConfigFileRepositoryPath
+    Copy-Item -Path "Repository:\Anyconnect.XML" -Destination $configFileDestinationVPN -Force
+    Copy-Item -Path "Repository:\OrgInfo.json" -Destination $configFileDestinationUmbrella -Force
+    #remove network drive
+Remove-PSDrive -Name "Repository"
+} 
+else {
+    Write-Output "Umbrella Configuration Present"
+}
